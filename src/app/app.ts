b96a7e4e11a08cdb155e84128db884045566d344
demo-09 <ujjdemo@gmail.com>
2026-02-11 00:37:39 +0530

    hero+nav+footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import {NavComponent} from './shared/navbar/nav.component/nav.component'
+import {HeroSectionComponent}from './shared/navbar/hero-section.component/hero-section.component'
 import { RouterOutlet } from '@angular/router';
-
+import { FooterComponent } from './shared/footer/footer.component/footer.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroSectionComponent,NavComponent,FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('farmEase');
+  
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./app/app').then(m => m.App) },
+];
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
